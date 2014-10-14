2ef83fd511fdcdc0eeb8a4ad6c042937aa3ba522
gilljam <felicia.gilljam@gmail.com>
2014-10-14 17:34:33 +0200

    Implemented Screenscraping method in ABScraper

--- ScreenScraper/ABScraper.cs
+++ ScreenScraper/ABScraper.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using HtmlAgilityPack;
 
 namespace ScreenScraper
 {
     public class ABScraper
     {
-        public string FindCompanyByOrgNo(string orgNo)
+        public string FindCompanyByOrgNo(string o)
         {
-            throw new NotImplementedException();
+            string orgNo = o.Replace("-", "");
+            var getHtmlWeb = new HtmlWeb();
+            var pageResult = getHtmlWeb.Load(String.Format("http://www.allabolag.se/" + orgNo));
+            var orgName = pageResult.DocumentNode.SelectSingleNode("//span[@id='printTitle']").InnerText;
+            return orgName;
         }
     }
 }
